b85e89a015e2d79fea01512f83d449ab757ed595
Yasmineltair <yasmineltair293@gmail.com>
2023-09-16 10:49:50 -0700

    update3

--- _printf.c
+++ _printf.c
@@ -17,30 +17,40 @@ va_start(list_of_args, format);
 while (*format)
 {
 	if (*format != '%')
+	{
 		write(1, format, 1);
 		char_print++;
+	}
 	else
 		format++;
 	if (*format == '\0')
 		break;
 	else if (*format == '%')
+	{
 		write(1, format, 1);
 		char_print++;
+	}
 	else if (*format == 'c')
+	{
 		char c = va_arg(list_of_args, int);
 
 		write(1, &c, 1);
 		char_print++;
+	}
 	else if (*format == 's')
+	{
 		char *str = va_arg(list_of_args, char*);
 		int str_len = 0;
 
 		while (str[str_len] != '\0')
+		{
 			str_len++;
 		write(1, str, str_len);
-		char_print += str_len}
+		char_print += str_len
+		}
 	format++;
 }
 va_end(list_of_args);
 return (char_print);
+}
 }
