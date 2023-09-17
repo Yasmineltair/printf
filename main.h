fc3ce391127ead653dffe33c1881051af350e0d9
Nurelmallah <nourhan.elmallah@gmail.com>
2023-09-17 07:09:36 -0700

    another trial

--- main.h
+++ main.h
@@ -13,6 +13,7 @@ int _printf(const char *format, ...);
 /* Function prototypes for additional print functions */
 void print_char(char c, int *char_print);
 void print_str(char *str, int *char_print);
+void print_int(int num, int *char_print);
 
 #endif /* MAIN_H */
 
