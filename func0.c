a4ed0911fb19c1a5069112c5203d0d59c88b11fc
Nurelmallah <nourhan.elmallah@gmail.com>
2023-09-17 05:49:39 -0700

    adding func0.c, edit main.h

---
+++ func0.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * get_function - function specifiers.
+ *
+ * @con_spec: Conversion specifiers.
+ * @args: arguments.
+ *
+ * Return: char count.
+ */
+
+int get_function(char con_spec, va_list args)
+{
+	int i = 0;
+	int count_fun = 0;
+
+	specifiers_t spec[] = {
+		{'d', print_digit},
+		{'i', print_digit},
+		{0, NULL}
+	};
+
+	while (spec[i].specifiers)
+	{
+		if (con_spec == spec[i].specifiers)
+			count_fun += spec[i].f(args);
+		i++;
+	}
+
+	if (count_fun == 0)
+	{
+		count_fun += _putchar('%');
+		count_fun += _putchar(con_spec);
+	}
+
+	return (count_fun);
+}
+
