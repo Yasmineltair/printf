64e4d0710a44c6cbb9b508b56a424f17f186ac0e
Yasmineltair <yasmineltair293@gmail.com>
2023-09-18 13:58:41 -0700

    update buff

--- func1.c
+++ func1.c
@@ -9,12 +9,14 @@ void print_char(char c, int *char_print)
 	int buff_ind = 0;
 	char buffer[BUFF_SIZE];
 
-buffer[buff_ind++] = 'c';
+buffer[buff_ind++] = c;
 if (buff_ind == BUFF_SIZE)
 {
 print_buffer(buffer, &buff_ind);
-char_print += buff_ind;
+*char_print += buff_ind;
 	}
+}
+
 /**
   * print_str - function to print string
   * @str: the string to print
@@ -25,11 +27,14 @@ void print_str(char *str, int *char_print)
 	int buff_ind = 0;
 	char buffer[BUFF_SIZE];
 
+while (*str)
+{
 buffer[buff_ind++] = *str;
 str++;
 if (buff_ind == BUFF_SIZE)
 {
 		print_buffer(buffer, &buff_ind);
+*char_print += buff_ind;
 }
-char_print += buff_ind;
+}
 }
