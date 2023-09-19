4e7bbae9996376cc608acbe2ac17cae067aaa991
Nurelmallah <nourhan.elmallah@gmail.com>
2023-09-19 13:56:19 -0700

    edit for betty

--- _printf.c
+++ _printf.c
@@ -8,37 +8,39 @@
 
 int _printf(const char *format, ...)
 {
-int char_print = 0;
-va_list list_of_args;
+	int char_print = 0;
+	va_list list_of_args;
 
-if (format == NULL || (format[0] == '%' && !format[1]))
+	if (format == NULL || (format[0] == '%' && !format[1]))
 	return (-1);
-va_start(list_of_args, format);
-while (*format)
-{
-	if (*format != '%')
-	{
-		print_char(*format, &char_print);
-	}
-	else
-	{
-		format++;
-	if (*format == '%')
-	{
-		 print_char(*format, &char_print);
-	}
-	else if (*format == 'c')
-	{
-		char c = va_arg(list_of_args, int);
 
-		print_char(c, &char_print);
-	}
-	else if (*format == 's')
-	{
-		char *str = va_arg(list_of_args, char*);
+	va_start(list_of_args, format);
 
-		print_str(str, char_print);
-	}
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			print_char(*format, &char_print);
+		}
+		else
+		{
+			format++;
+		if (*format == '%')
+		{
+			print_char(*format, &char_print);
+		}
+		else if (*format == 'c')
+		{
+			char c = va_arg(list_of_args, int);
+
+			print_char(c, &char_print);
+		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(list_of_args, char*);
+
+			print_str(str, char_print);
+		}
 	}
 	format++;
 }
